0ec135393d23e732a6307da031e7949e54b4c1ea
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-02-18 13:15:11 -0500

    Finished Palindrome

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -3,6 +3,54 @@ package com.zipcodewilmington.assessment1.part5;
 public class Palindrome {
 
     public Integer countPalindromes(String input){
-        return null;
+        String[] arrayOfSubStrings = getArrayOfAllSubstrings(input);
+        int countOfPalindromes = 0;
+
+        for (String s : arrayOfSubStrings) {
+            if (isPalindrome(s)) {
+                countOfPalindromes++;
+            }
+        }
+
+        return countOfPalindromes;
+    }
+
+    private static String[] getArrayOfAllSubstrings(String input) {
+        int arraySize = (input.length() + 1) * input.length() / 2;
+        // The number of distinct non-empty substrings in a string (including the full string) is equal to the sum of
+        // all integers from 1 up to the number of characters in the string. Using Euler's method of summing numbers
+        // from 1 to n, we can reduce this to a simple equation. (n + 1) * (n / 2)
+
+        String[] arrayOfSubstrings = new String[arraySize]; // initializing the array to the correct size
+        int index = 0; // keeping track of next index
+
+        // The following nested for loop iterates through every possible substring and assigns each one to the arrayOfSubstrings
+        for (int startIndex = 0; startIndex < input.length(); startIndex++) {
+            for (int endIndex = startIndex + 1; endIndex <= input.length(); endIndex++){
+                arrayOfSubstrings[index] = input.substring(startIndex, endIndex);
+                index++;
+            }
+        }
+
+        return arrayOfSubstrings;
+    }
+
+    /**
+     * Checks to see if the input String is a palindrome
+     *
+     * @param input string to check to see if its a palindrome
+     * @return true if the input String is a palindrome, false otherwise
+     */
+    private static boolean isPalindrome(String input) {
+        boolean result = false;
+
+        StringBuilder reverser = new StringBuilder(input);
+        String reversedString = reverser.reverse().toString();
+
+        if (input.equals(reversedString)) {
+            result = true;
+        }
+
+        return result;
     }
 }
