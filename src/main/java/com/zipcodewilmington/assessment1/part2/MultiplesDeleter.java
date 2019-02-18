40012693b5eff3f30beaf83760a5f9d34405b5c5
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-02-18 11:16:11 -0500

    Finished MultiplesDeleter

--- src/main/java/com/zipcodewilmington/assessment1/part2/MultiplesDeleter.java
+++ src/main/java/com/zipcodewilmington/assessment1/part2/MultiplesDeleter.java
@@ -1,5 +1,8 @@
 package com.zipcodewilmington.assessment1.part2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by leon on 2/16/18.
  */
@@ -10,7 +13,7 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with evens removed
      */
     public Integer[] deleteEvens(Integer[] ints) {
-        return null;
+        return deleteMultiplesOfN(ints, 2);
     }
 
     /**
@@ -19,7 +22,7 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with odds removed
      */
     public Integer[] deleteOdds(Integer[] ints) {
-        return null;
+        return deleteNonMultiplesOfN(ints, 2);
     }
 
     /**
@@ -28,7 +31,7 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with numbers indivisible by 3 removed
      */
     public Integer[] deleteMultiplesOf3(Integer[] ints) {
-        return null;
+        return deleteMultiplesOfN(ints, 3);
     }
 
     /**
@@ -38,6 +41,32 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with numbers indivisible by `multiple` removed
      */
     public Integer[] deleteMultiplesOfN(Integer[] ints, int multiple) {
-        return null;
+        ArrayList<Integer> deleteEvensArrayList = new ArrayList<>(Arrays.asList(ints));
+        int index = 0;
+
+        while (index < deleteEvensArrayList.size()) {
+            if (Math.floorMod(deleteEvensArrayList.get(index), multiple) == 0) {
+                deleteEvensArrayList.remove(index);
+                index--;
+            }
+            index++;
+        }
+
+        return deleteEvensArrayList.toArray(new Integer[0]);
+    }
+
+    public Integer[] deleteNonMultiplesOfN(Integer[] ints, int multiple) {
+        ArrayList<Integer> deleteEvensArrayList = new ArrayList<>(Arrays.asList(ints));
+        int index = 0;
+
+        while (index < deleteEvensArrayList.size()) {
+            if (Math.floorMod(deleteEvensArrayList.get(index), multiple) != 0) {
+                deleteEvensArrayList.remove(index);
+                index--;
+            }
+            index++;
+        }
+
+        return deleteEvensArrayList.toArray(new Integer[0]);
     }
 }
