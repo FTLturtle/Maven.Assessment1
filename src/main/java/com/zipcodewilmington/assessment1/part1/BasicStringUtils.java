f9ebb6a94994d63b4a4a81feef0563fe875b532f
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-02-18 09:39:21 -0500

    Finished BasicStringUtils

--- src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
@@ -9,7 +9,7 @@ public class BasicStringUtils {
      * @return string with identical content, and the first character capitalized
      */
     public static String camelCase(String str) {
-        return null;
+        return Character.toUpperCase(str.charAt(0)) + str.substring(1);
     }
 
     /**
@@ -17,7 +17,8 @@ public class BasicStringUtils {
      * @return string with identical contents, in the reverse order
      */
     public static String reverse(String str) {
-        return null;
+        StringBuilder reverser = new StringBuilder(str);
+        return reverser.reverse().toString();
     }
 
     /**
@@ -25,7 +26,7 @@ public class BasicStringUtils {
      * @return string with identical contents, in reverse order, with first character capitalized
      */
     public static String reverseThenCamelCase(String str) {
-        return null;
+        return camelCase(reverse(str));
     }
 
 
@@ -34,7 +35,7 @@ public class BasicStringUtils {
      * @return string with identical contents excluding first and last character
      */
     public static String removeFirstAndLastCharacter(String str) {
-        return null;
+        return str.substring(1, str.length() - 1);
     }
 
     /**
@@ -42,6 +43,17 @@ public class BasicStringUtils {
      * @return string with identical characters, each with opposite casing
      */
     public static String invertCasing(String str) {
-        return null;
+        StringBuilder casingInverter = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++){
+            if (Character.isUpperCase(str.charAt(i))) {
+                casingInverter.append(Character.toLowerCase(str.charAt(i)));
+            } else if (Character.isLowerCase(str.charAt(i))) {
+                casingInverter.append(Character.toUpperCase(str.charAt(i)));
+            } else {
+                casingInverter.append(str.charAt(i));
+            }
+        }
+        return casingInverter.toString();
     }
 }
